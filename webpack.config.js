779bc61310e5670b80aa9ce0f20cd477f053e6e1
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-10-01 20:59:36 +0200

    environment configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 const path = require('path');
 
 //webpack.config.js
-module.exports = {
+module.exports = (env) => {
+  const environment = env || 'production';
+
+  return {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.' + environment + '.bundle.js'
     },
 
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [/node_modules/],
-                loader: "babel-loader"
-            }
-        ]
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: [/node_modules/],
+          loader: "babel-loader"
+        }
+      ]
     }
+  }
 };
