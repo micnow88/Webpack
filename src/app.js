da8ce2326eddd0b3c90448ab517316954a434b06
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-10-02 13:36:44 +0200

    add plugins

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
 // app.js
 import world from './world';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 console.log('hello ' + world);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = name || 'Michał';
+  }
+
+  render() {
+    return (
+      <div>
+        <p>'hello ' + world</p>
+        <p>'hello ' + {name}</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const webpack = require('webpack');
 const path = require('path');
+
+const plugins = [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
 //webpack.config.js
 module.exports = (env) => {
   const environment = env || 'production';
+
+  if (env === 'production') {
+    plugins.push(
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    )
+  }
 
   return {
     entry: './src/app.js',
@@ -19,6 +36,8 @@ module.exports = (env) => {
           loader: "babel-loader"
         }
       ]
-    }
+    },
+
+    plugins: plugins //może być po prostu plugins!
   }
 };
